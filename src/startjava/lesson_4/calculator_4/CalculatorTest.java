c050795fbc783d3953993b1c402ac23371a00379
Andrew GRBNV <stolet82@gmail.com>
2020-11-21 00:26:40 +0300

    4 lesson

--- src/startjava/lesson_4/calculator_4/CalculatorTest.java
+++ src/startjava/lesson_4/calculator_4/CalculatorTest.java
@@ -1,48 +1,44 @@
-package src.startjava.lesson_2_3.calculator_2;
+package src.startjava.lesson_4.calculator_4;
+
 import java.util.Scanner;
 
 public class CalculatorTest {
 
 
     public static void main(String[] args) {
-        Calculator_2 calc = new Calculator_2();
         do {
-            double num1 = inputNumber();
-            char operation = inputOperation();
-            double num2 = inputNumber();
-            double result = calc.calculate(num1, num2, operation);
-            System.out.println("Результат равен " + result);
-        } while (!isNext());
-    }
+            System.out.println("Введите два числа и операцию с пробелами между ними");
 
-    public static double inputNumber() {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Введите число ");
-        if (scan.hasNextDouble()) {
-            return scan.nextDouble();
-        }
-        System.out.println("Число введено не корректно, введите число ");
-        return inputNumber();
+            Scanner scanner = new Scanner(System.in);
+            String calculation = scanner.nextLine();
+            System.out.println("Ответ: " + calculate(calculation));
+        } while (isNext());
     }
+//
+    public static double calculate(String calculation) {
+        String[] arr = calculation.split(" ");
 
-    public static char inputOperation() {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Введите действие:");
-        System.out.println("+ Сложение");
-        System.out.println("- Вычитание ");
-        System.out.println("* Умножение ");
-        System.out.println("/ Деление");
-        System.out.println("^ Возведение в степень");
-        System.out.println("% Остаток от деления");
-        char operation;
-        if (scan.hasNext()) {
-            operation = scan.next().charAt(0);
-            if (operation == '+' || operation == '-' || operation == '*' || operation == '/' || operation == '^' || operation == '%') {
-                return operation;
-            }
+        int result = 0;
+        int data0 = Integer.parseInt(arr[0]);
+        int data1 = Integer.parseInt(arr[2]);
+
+        String operation = arr[1];
+
+        switch (operation) {
+            case "+":
+                return data0 + data1;
+            case "-":
+                return data0 - data1;
+            case "*":
+                return data0 * data1;
+            case "/":
+                return data0 / data1;
+            case "^":
+                return Math.pow( data0,data1);
+            default:
+                System.out.println("Неверный оператор: " + operation);
+                return result;
         }
-        System.out.println("Вы ввели не правильную операцию, введите ");
-        return inputOperation();
     }
 
     public static boolean isNext() {
@@ -59,6 +55,6 @@ public class CalculatorTest {
             }
         }
         return isNext();
-    }
+   }
 
 }
